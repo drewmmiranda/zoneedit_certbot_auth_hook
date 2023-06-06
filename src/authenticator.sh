5bb63153eba66ad33d468933a867e48eee873c2d
Drew Miranda <drewmiranda@gmail.com>
2023-06-06 17:46:24 -0500

    more comments

--- src/authenticator.sh
+++ src/authenticator.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# Get your API key from https://www.cloudflare.com/a/account/my-account
+# Zoneedit username
 USERNAME=""
+# Zoneedit Dynamic token
+# See https://support.zoneedit.com/en/knowledgebase/article/dynamic-dns
 API_KEY=""
 
 # Strip only the top domain to get the zone id
 # This does not work. CERTBOT_DOMAIN is already only the domain without any subdomains
 # The below retuns "" if CERTBOT_DOMAIN is a domain name, which in my limited testing it always is
-DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
+# DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
 
+# Variables obtained from certbot
 echo CERTBOT_DOMAIN: $CERTBOT_DOMAIN
-echo DOMAIN: $DOMAIN
+# echo DOMAIN: $DOMAIN
 echo CERTBOT_VALIDATION: $CERTBOT_VALIDATION
 
 # Create TXT record
